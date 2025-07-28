5d1090ea64f843b70d784b3322f42c60247323f9
mark9763kmcd <mark9763kmcd@gmail.com>
2025-07-28 09:22:54 -0400

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ function redirectToLink() {
     localStorage.setItem("cookieConsent", "false"); // Reset consent
     setTimeout(() => {
       window.location.href = "https://google.com";
-    }, 700000); // 7 seconds
+    }, 5000); // 7 seconds
   }
 }
 
